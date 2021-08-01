f37c23858b7ec588607817383c43b7af341ef7f8
Rupesh0 <pandeyrupesh00@gmail.com>
2021-08-01 21:43:38 +0530

    First Commit

---
+++ app/src/main/java/com/example/meetingmanagement/MeetingData.java
@@ -0,0 +1,35 @@
+package com.example.meetingmanagement;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+public class MeetingData extends SQLiteOpenHelper {
+
+    public static String DBName="meeting.db";
+
+    public MeetingData(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+
+
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table User(loginID TEXT primary key, password TEXT)");
+        db.execSQL("create table MeetingPortfolio(portfolioID TEXT,foreign key(portfolioID) references User(loginID))");
+        db.execSQL("create table Meetings(portfolioID TEXT,meetingDate TEXT, foreign key(portfolioID) references MeetingPortfolio(portfolioID))");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/meetingmanagement/MeetingMain.java
@@ -0,0 +1,25 @@
+package com.example.meetingmanagement;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MeetingMain extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
